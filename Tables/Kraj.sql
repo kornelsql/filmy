deacca0fcc7f7171650df27511f1a27757d21849
korneliusz <kolaw29@gmail.com>
2024-10-23 20:52:11 +0200

    Revert "remove bracket in kraj table"
    This reverts commit f654554e8dfd5a88790973e2c843f6ff4576f62e.

--- Tables/Kraj.sql
+++ Tables/Kraj.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [dbo].[Kraj] (
-    KrajID INT            IDENTITY (1, 1) NOT NULL,
-    Kraj   NVARCHAR (100) NOT NULL,
+    [KrajID] INT            IDENTITY (1, 1) NOT NULL,
+    [Kraj]   NVARCHAR (100) NOT NULL,
     PRIMARY KEY CLUSTERED ([KrajID] ASC)
 );
 
